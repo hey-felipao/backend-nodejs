8ae44027c4b4b217cc7e0f738b17a84a6cf6b79d
Felipe Fernandes <hey_felipao@gmail.com>
2020-01-30 10:37:19 -0300

    change structure of directories

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
-require("dotenv").config({ encoding: "utf8" });
+dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
-//Carregando o Express
 const app = express();
 
 app.use(bodyParser.text());
@@ -14,10 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+//chamar rota para pasta rota
 app.use("/", require("../routes"));
 
-app.listen(PORT, (req, res) => {
-  console.log("App is running at http://localhost:%d", PORT);
-});
+app.set("port", process.env.APP_PORT || 5000);
 
 module.exports = { app };
