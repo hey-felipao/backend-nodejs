b4b4fdbac44ed3eb4b2f0dff0ffe2d0c10f9c1d1
Felipe Fernandes <hey_felipao@gmail.com>
2020-01-29 22:27:15 -0300

    route example

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const routes = require("express").Router();
 const cors = require("cors");
+//const customer = require ( './customer' );
+
+//routes.use( '/nome_da_rota', cors(), customer);
 
 routes.get("/", (req, res) => {
   res.send("Hello World!");
